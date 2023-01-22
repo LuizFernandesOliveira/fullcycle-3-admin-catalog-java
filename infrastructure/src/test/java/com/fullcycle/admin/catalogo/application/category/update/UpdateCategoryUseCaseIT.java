616c98cfc1c362ac9aac952fe0b0eb1bb71bb115
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2023-01-21 21:01:39 -0300

    feat: add test update category usecase integration

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseIT.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseIT.java
@@ -3,202 +3,199 @@ package com.fullcycle.admin.catalogo.application.category.update;
 import com.fullcycle.admin.catalogo.IntegrationTest;
 import com.fullcycle.admin.catalogo.domain.category.Category;
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
-import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
 import com.fullcycle.admin.catalogo.domain.exceptions.NotFoundException;
 import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @IntegrationTest
 public class UpdateCategoryUseCaseIT {
 
-    @Autowired
-    private UpdateCategoryUseCase useCase;
+  @Autowired
+  private UpdateCategoryUseCase useCase;
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+  @Autowired
+  private CategoryRepository categoryRepository;
 
-    @SpyBean
-    private CategoryGateway categoryGateway;
+  @SpyBean
+  private CategoryGateway categoryGateway;
 
-    @Test
-    public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() {
-        final var aCategory =
-                Category.newCategory("Film", null, true);
+  @Test
+  public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() {
+    final var aCategory =
+        Category.newCategory("Film", null, true);
 
-        save(aCategory);
+    save(aCategory);
 
-        final var expectedName = "Filmes";
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = true;
-        final var expectedId = aCategory.getId();
+    final var expectedName = "Filmes";
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
+    final var expectedId = aCategory.getId();
 
-        final var aCommand = UpdateCategoryCommand.with(
-                expectedId.getValue(),
-                expectedName,
-                expectedDescription,
-                expectedIsActive
-        );
+    final var aCommand = UpdateCategoryCommand.with(
+        expectedId.getValue(),
+        expectedName,
+        expectedDescription,
+        expectedIsActive
+    );
 
-        Assertions.assertEquals(1, categoryRepository.count());
+    assertEquals(1, categoryRepository.count());
 
-        final var actualOutput = useCase.execute(aCommand).get();
+    final var actualOutput = useCase.execute(aCommand).get();
 
-        Assertions.assertNotNull(actualOutput);
-        Assertions.assertNotNull(actualOutput.id());
+    assertNotNull(actualOutput);
+    assertNotNull(actualOutput.id());
 
-        final var actualCategory =
-                categoryRepository.findById(expectedId.getValue()).get();
+    final var actualCategory =
+        categoryRepository.findById(expectedId.getValue()).get();
 
-        Assertions.assertEquals(expectedName, actualCategory.getName());
-        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
-        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
-        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
-        Assertions.assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
-        Assertions.assertNull(actualCategory.getDeletedAt());
-    }
+    assertEquals(expectedName, actualCategory.getName());
+    assertEquals(expectedDescription, actualCategory.getDescription());
+    assertEquals(expectedIsActive, actualCategory.isActive());
+    assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+    assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
+    assertNull(actualCategory.getDeletedAt());
+  }
 
-    @Test
-    public void givenAInvalidName_whenCallsUpdateCategory_thenShouldReturnDomainException() {
-        final var aCategory =
-                Category.newCategory("Film", null, true);
+  @Test
+  public void givenAInvalidName_whenCallsUpdateCategory_thenShouldReturnDomainException() {
+    final var aCategory =
+        Category.newCategory("Film", null, true);
 
-        save(aCategory);
+    save(aCategory);
 
-        final String expectedName = null;
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = true;
-        final var expectedId = aCategory.getId();
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
+    final var expectedId = aCategory.getId();
 
-        final var expectedErrorMessage = "'name' should not be null";
-        final var expectedErrorCount = 1;
+    final var expectedErrorMessage = "'name' should not be null";
+    final var expectedErrorCount = 1;
 
-        final var aCommand =
-                UpdateCategoryCommand.with(expectedId.getValue(), expectedName, expectedDescription, expectedIsActive);
+    final var aCommand =
+        UpdateCategoryCommand.with(expectedId.getValue(), null, expectedDescription, expectedIsActive);
 
-        final var notification = useCase.execute(aCommand).getLeft();
+    final var notification = useCase.execute(aCommand).getLeft();
 
-        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+    assertEquals(expectedErrorCount, notification.getErrors().size());
+    assertEquals(expectedErrorMessage, notification.firstError().message());
 
-        Mockito.verify(categoryGateway, times(0)).update(any());
-    }
+    verify(categoryGateway, times(0)).update(any());
+  }
 
-    @Test
-    public void givenAValidInactivateCommand_whenCallsUpdateCategory_shouldReturnInactiveCategoryId() {
-        final var aCategory =
-                Category.newCategory("Film", null, true);
+  @Test
+  public void givenAValidInactivateCommand_whenCallsUpdateCategory_shouldReturnInactiveCategoryId() {
+    final var aCategory =
+        Category.newCategory("Film", null, true);
 
-        save(aCategory);
+    save(aCategory);
 
-        final var expectedName = "Filmes";
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = false;
-        final var expectedId = aCategory.getId();
+    final var expectedName = "Filmes";
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = false;
+    final var expectedId = aCategory.getId();
 
-        final var aCommand = UpdateCategoryCommand.with(
-                expectedId.getValue(),
-                expectedName,
-                expectedDescription,
-                expectedIsActive
-        );
+    final var aCommand = UpdateCategoryCommand.with(
+        expectedId.getValue(),
+        expectedName,
+        expectedDescription,
+        expectedIsActive
+    );
 
-        Assertions.assertTrue(aCategory.isActive());
-        Assertions.assertNull(aCategory.getDeletedAt());
+    assertTrue(aCategory.isActive());
+    assertNull(aCategory.getDeletedAt());
 
-        final var actualOutput = useCase.execute(aCommand).get();
+    final var actualOutput = useCase.execute(aCommand).get();
 
-        Assertions.assertNotNull(actualOutput);
-        Assertions.assertNotNull(actualOutput.id());
+    assertNotNull(actualOutput);
+    assertNotNull(actualOutput.id());
 
-        final var actualCategory =
-                categoryRepository.findById(expectedId.getValue()).get();
+    final var actualCategory =
+        categoryRepository.findById(expectedId.getValue()).get();
 
-        Assertions.assertEquals(expectedName, actualCategory.getName());
-        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
-        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
-        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
-        Assertions.assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
-        Assertions.assertNotNull(actualCategory.getDeletedAt());
-    }
+    assertEquals(expectedName, actualCategory.getName());
+    assertEquals(expectedDescription, actualCategory.getDescription());
+    assertEquals(expectedIsActive, actualCategory.isActive());
+    assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+    assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
+    assertNotNull(actualCategory.getDeletedAt());
+  }
 
-    @Test
-    public void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAException() {
-        final var aCategory =
-                Category.newCategory("Film", null, true);
+  @Test
+  public void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAException() {
+    final var aCategory =
+        Category.newCategory("Film", null, true);
 
-        save(aCategory);
+    save(aCategory);
 
-        final var expectedName = "Filmes";
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = true;
-        final var expectedId = aCategory.getId();
-        final var expectedErrorCount = 1;
-        final var expectedErrorMessage = "Gateway error";
+    final var expectedName = "Filmes";
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
+    final var expectedId = aCategory.getId();
+    final var expectedErrorCount = 1;
+    final var expectedErrorMessage = "Gateway error";
 
-        final var aCommand = UpdateCategoryCommand.with(
-                expectedId.getValue(),
-                expectedName,
-                expectedDescription,
-                expectedIsActive
-        );
+    final var aCommand = UpdateCategoryCommand.with(
+        expectedId.getValue(),
+        expectedName,
+        expectedDescription,
+        expectedIsActive
+    );
 
-        doThrow(new IllegalStateException(expectedErrorMessage))
-                .when(categoryGateway).update(any());
+    doThrow(new IllegalStateException(expectedErrorMessage))
+        .when(categoryGateway).update(any());
 
-        final var notification = useCase.execute(aCommand).getLeft();
+    final var notification = useCase.execute(aCommand).getLeft();
 
-        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+    assertEquals(expectedErrorCount, notification.getErrors().size());
+    assertEquals(expectedErrorMessage, notification.firstError().message());
 
-        final var actualCategory =
-                categoryRepository.findById(expectedId.getValue()).get();
+    final var actualCategory =
+        categoryRepository.findById(expectedId.getValue()).get();
 
-        Assertions.assertEquals(aCategory.getName(), actualCategory.getName());
-        Assertions.assertEquals(aCategory.getDescription(), actualCategory.getDescription());
-        Assertions.assertEquals(aCategory.isActive(), actualCategory.isActive());
-        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
-        Assertions.assertEquals(aCategory.getUpdatedAt(), actualCategory.getUpdatedAt());
-        Assertions.assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
-    }
+    assertEquals(aCategory.getName(), actualCategory.getName());
+    assertEquals(aCategory.getDescription(), actualCategory.getDescription());
+    assertEquals(aCategory.isActive(), actualCategory.isActive());
+    assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+    assertEquals(aCategory.getUpdatedAt(), actualCategory.getUpdatedAt());
+    assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
+  }
 
-    @Test
-    public void givenACommandWithInvalidID_whenCallsUpdateCategory_shouldReturnNotFoundException() {
-        final var expectedName = "Filmes";
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = false;
-        final var expectedId = "123";
-        final var expectedErrorCount = 1;
-        final var expectedErrorMessage = "Category with ID 123 was not found";
+  @Test
+  public void givenACommandWithInvalidID_whenCallsUpdateCategory_shouldReturnNotFoundException() {
+    final var expectedName = "Filmes";
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = false;
+    final var expectedId = "123";
+    final var expectedErrorMessage = "Category with ID 123 was not found";
 
-        final var aCommand = UpdateCategoryCommand.with(
-                expectedId,
-                expectedName,
-                expectedDescription,
-                expectedIsActive
-        );
+    final var aCommand = UpdateCategoryCommand.with(
+        expectedId,
+        expectedName,
+        expectedDescription,
+        expectedIsActive
+    );
 
-        final var actualException =
-                Assertions.assertThrows(NotFoundException.class, () -> useCase.execute(aCommand));
+    final var actualException =
+        assertThrows(NotFoundException.class, () -> useCase.execute(aCommand));
 
-        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
-    }
+    assertEquals(expectedErrorMessage, actualException.getMessage());
+  }
 
-    private void save(final Category... aCategory) {
-        categoryRepository.saveAllAndFlush(
-                Arrays.stream(aCategory)
-                        .map(CategoryJpaEntity::from)
-                        .toList()
-        );
-    }
+  private void save(final Category... aCategory) {
+    categoryRepository.saveAllAndFlush(
+        Arrays.stream(aCategory)
+            .map(CategoryJpaEntity::from)
+            .toList()
+    );
+  }
 }
