54605e8198c7d7024c7d0713d4cca5209b2089a3
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2023-01-21 21:44:27 -0300

    feat: add exception handler

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -13,19 +13,19 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = NotFoundException.class)
-    public ResponseEntity<?> handleNotFoundException(final NotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiError.from(ex));
-    }
+  @ExceptionHandler(value = NotFoundException.class)
+  public ResponseEntity<?> handleNotFoundException(final NotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiError.from(ex));
+  }
 
-    @ExceptionHandler(value = DomainException.class)
-    public ResponseEntity<?> handleDomainException(final DomainException ex) {
-        return ResponseEntity.unprocessableEntity().body(ApiError.from(ex));
-    }
+  @ExceptionHandler(value = DomainException.class)
+  public ResponseEntity<?> handleDomainException(final DomainException ex) {
+    return ResponseEntity.unprocessableEntity().body(ApiError.from(ex));
+  }
 
-    record ApiError(String message, List<Error> errors) {
-        static ApiError from(final DomainException ex) {
-            return new ApiError(ex.getMessage(), ex.getErrors());
-        }
+  record ApiError(String message, List<Error> errors) {
+    static ApiError from(final DomainException ex) {
+      return new ApiError(ex.getMessage(), ex.getErrors());
     }
+  }
 }
