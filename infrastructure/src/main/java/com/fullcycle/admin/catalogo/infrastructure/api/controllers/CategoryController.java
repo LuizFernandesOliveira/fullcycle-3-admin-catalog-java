5a1233327526921a2869f1feb2296e43f72a7f80
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2023-01-21 21:23:02 -0300

    feat: add controller category

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -28,82 +28,82 @@ import java.util.function.Function;
 @RestController
 public class CategoryController implements CategoryAPI {
 
-    private final CreateCategoryUseCase createCategoryUseCase;
-    private final GetCategoryByIdUseCase getCategoryByIdUseCase;
-    private final UpdateCategoryUseCase updateCategoryUseCase;
-    private final DeleteCategoryUseCase deleteCategoryUseCase;
-    private final ListCategoriesUseCase listCategoriesUseCase;
+  private final CreateCategoryUseCase createCategoryUseCase;
+  private final GetCategoryByIdUseCase getCategoryByIdUseCase;
+  private final UpdateCategoryUseCase updateCategoryUseCase;
+  private final DeleteCategoryUseCase deleteCategoryUseCase;
+  private final ListCategoriesUseCase listCategoriesUseCase;
 
-    public CategoryController(
-            final CreateCategoryUseCase createCategoryUseCase,
-            final GetCategoryByIdUseCase getCategoryByIdUseCase,
-            final UpdateCategoryUseCase updateCategoryUseCase,
-            final DeleteCategoryUseCase deleteCategoryUseCase,
-            final ListCategoriesUseCase listCategoriesUseCase
-    ) {
-        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
-        this.getCategoryByIdUseCase = Objects.requireNonNull(getCategoryByIdUseCase);
-        this.updateCategoryUseCase = Objects.requireNonNull(updateCategoryUseCase);
-        this.deleteCategoryUseCase = Objects.requireNonNull(deleteCategoryUseCase);
-        this.listCategoriesUseCase = Objects.requireNonNull(listCategoriesUseCase);
-    }
+  public CategoryController(
+      final CreateCategoryUseCase createCategoryUseCase,
+      final GetCategoryByIdUseCase getCategoryByIdUseCase,
+      final UpdateCategoryUseCase updateCategoryUseCase,
+      final DeleteCategoryUseCase deleteCategoryUseCase,
+      final ListCategoriesUseCase listCategoriesUseCase
+  ) {
+    this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    this.getCategoryByIdUseCase = Objects.requireNonNull(getCategoryByIdUseCase);
+    this.updateCategoryUseCase = Objects.requireNonNull(updateCategoryUseCase);
+    this.deleteCategoryUseCase = Objects.requireNonNull(deleteCategoryUseCase);
+    this.listCategoriesUseCase = Objects.requireNonNull(listCategoriesUseCase);
+  }
 
-    @Override
-    public ResponseEntity<?> createCategory(final CreateCategoryRequest input) {
-        final var aCommand = CreateCategoryCommand.with(
-                input.name(),
-                input.description(),
-                input.active() != null ? input.active() : true
-        );
+  @Override
+  public ResponseEntity<?> createCategory(final CreateCategoryRequest input) {
+    final var aCommand = CreateCategoryCommand.with(
+        input.name(),
+        input.description(),
+        input.active() != null ? input.active() : true
+    );
 
-        final Function<Notification, ResponseEntity<?>> onError = notification ->
-                ResponseEntity.unprocessableEntity().body(notification);
+    final Function<Notification, ResponseEntity<?>> onError = notification ->
+        ResponseEntity.unprocessableEntity().body(notification);
 
-        final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess = output ->
-                ResponseEntity.created(URI.create("/categories/" + output.id())).body(output);
+    final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess = output ->
+        ResponseEntity.created(URI.create("/categories/" + output.id())).body(output);
 
-        return this.createCategoryUseCase.execute(aCommand)
-                .fold(onError, onSuccess);
-    }
+    return this.createCategoryUseCase.execute(aCommand)
+        .fold(onError, onSuccess);
+  }
 
-    @Override
-    public Pagination<CategoryListResponse> listCategories(
-            final String search,
-            final int page,
-            final int perPage,
-            final String sort,
-            final String direction
-    ) {
-        return listCategoriesUseCase.execute(new SearchQuery(page, perPage, search, sort, direction))
-                .map(CategoryApiPresenter::present);
-    }
+  @Override
+  public Pagination<CategoryListResponse> listCategories(
+      final String search,
+      final int page,
+      final int perPage,
+      final String sort,
+      final String direction
+  ) {
+    return listCategoriesUseCase.execute(new SearchQuery(page, perPage, search, sort, direction))
+        .map(CategoryApiPresenter::present);
+  }
 
-    @Override
-    public CategoryResponse getById(final String id) {
-        return CategoryApiPresenter.present(this.getCategoryByIdUseCase.execute(id));
-    }
+  @Override
+  public CategoryResponse getById(final String id) {
+    return CategoryApiPresenter.present(this.getCategoryByIdUseCase.execute(id));
+  }
 
-    @Override
-    public ResponseEntity<?> updateById(final String id, final UpdateCategoryRequest input) {
-        final var aCommand = UpdateCategoryCommand.with(
-                id,
-                input.name(),
-                input.description(),
-                input.active() != null ? input.active() : true
-        );
+  @Override
+  public ResponseEntity<?> updateById(final String id, final UpdateCategoryRequest input) {
+    final var aCommand = UpdateCategoryCommand.with(
+        id,
+        input.name(),
+        input.description(),
+        input.active() != null ? input.active() : true
+    );
 
-        final Function<Notification, ResponseEntity<?>> onError = notification ->
-                ResponseEntity.unprocessableEntity().body(notification);
+    final Function<Notification, ResponseEntity<?>> onError = notification ->
+        ResponseEntity.unprocessableEntity().body(notification);
 
-        final Function<UpdateCategoryOutput, ResponseEntity<?>> onSuccess =
-                ResponseEntity::ok;
+    final Function<UpdateCategoryOutput, ResponseEntity<?>> onSuccess =
+        ResponseEntity::ok;
 
-        return this.updateCategoryUseCase.execute(aCommand)
-                .fold(onError, onSuccess);
-    }
+    return this.updateCategoryUseCase.execute(aCommand)
+        .fold(onError, onSuccess);
+  }
 
-    @Override
-    public void deleteById(final String anId) {
-        this.deleteCategoryUseCase.execute(anId);
-    }
+  @Override
+  public void deleteById(final String anId) {
+    this.deleteCategoryUseCase.execute(anId);
+  }
 }
