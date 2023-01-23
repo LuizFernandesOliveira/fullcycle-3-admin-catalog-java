73aeb4a657e66afc4b5fa23d7cb69feb6da7de63
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2023-01-23 19:33:39 -0300

    feat: add presenters

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/presenters/CategoryApiPresenter.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/presenters/CategoryApiPresenter.java
@@ -7,26 +7,26 @@ import com.fullcycle.admin.catalogo.infrastructure.category.models.CategoryListR
 
 public interface CategoryApiPresenter {
 
-    static CategoryResponse present(final CategoryOutput output) {
-        return new CategoryResponse(
-                output.id().getValue(),
-                output.name(),
-                output.description(),
-                output.isActive(),
-                output.createdAt(),
-                output.updatedAt(),
-                output.deletedAt()
-        );
-    }
+  static CategoryResponse present(final CategoryOutput output) {
+    return new CategoryResponse(
+        output.id().getValue(),
+        output.name(),
+        output.description(),
+        output.isActive(),
+        output.createdAt(),
+        output.updatedAt(),
+        output.deletedAt()
+    );
+  }
 
-    static CategoryListResponse present(final CategoryListOutput output) {
-        return new CategoryListResponse(
-                output.id().getValue(),
-                output.name(),
-                output.description(),
-                output.isActive(),
-                output.createdAt(),
-                output.deletedAt()
-        );
-    }
+  static CategoryListResponse present(final CategoryListOutput output) {
+    return new CategoryListResponse(
+        output.id().getValue(),
+        output.name(),
+        output.description(),
+        output.isActive(),
+        output.createdAt(),
+        output.deletedAt()
+    );
+  }
 }
